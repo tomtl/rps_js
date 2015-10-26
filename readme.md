8c34587e050af7313ed869eacb7c3b55dcea1ba0
Tom Lee <tom_t_lee@yahoo.com>
2015-10-26 13:14:41 +0000

    Git init

---
+++ readme.md
@@ -0,0 +1,5 @@
+Rock paper scissors with javascript
+===================================
+
+This is a rock, paper, scissors game built using javascript.  
+It's really simple.  
---
+++ rps.js
@@ -0,0 +1,39 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?").toLowerCase();
+
+var computerChoice = Math.random();
+
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+       return "The result is a tie!"; 
+    } else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "rock wins";
+        } else {
+            return "paper wins";
+        }
+    } else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "paper wins";
+        } else {
+            return "scissors wins";
+        }
+    } else if (choice1 === "scissors") {
+        if (choice2 === "paper") {
+            return "scissors win";
+        } else {
+            return "rock wins";
+        }
+    } else {
+        return "Invalid choice. Please enter 'rock', 'paper' or 'scissors'."
+    }
+};
+
+compare(userChoice, computerChoice)
