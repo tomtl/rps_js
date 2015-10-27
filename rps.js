841fb144f2099df282d8a5c66375d16600c03ab3
Tom Lee <tom_t_lee@yahoo.com>
2015-10-27 18:20:39 +0000

    Refactor functions

--- rps.js
+++ rps.js
@@ -1,34 +1,38 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?").toLowerCase();
+var computerChoice = computerChooses();
+compare(userChoice, computerChoice)
 
-if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
-    while (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
-        var userChoice = prompt("Invalid input. Do you choose 'rock', 'paper' or 'scissors'?").toLowerCase();
+function validateUserChoice(userChoice) {
+    if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+        while (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+            var userChoice = prompt("Invalid input. Do you choose 'rock', 'paper' or 'scissors'?").toLowerCase();
+        }
     }
-}
-
-var computerChoice = Math.random() * 3;
-if (computerChoice < 1) {
-	computerChoice = "rock";
-} else if(computerChoice < 2) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
-
-var winningCombinations = {
-    rock: "scissors",
-    scissors: "paper",
-    paper: "rock"
 };
 
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
+function computerChooses() {
+    var computerChoice = Math.random() * 3;
+    if (computerChoice < 1) {
+    	return "rock";
+    } else if(computerChoice < 2) {
+    	return "paper";
+    } else {
+    	return "scissors";
+    }
+};
+
+function compare(userChoice, computerChoice) {
+    var winningCombinations = {
+        rock: "scissors",
+        scissors: "paper",
+        paper: "rock"
+    };
+
+    if (userChoice === computerChoice) {
        return "The result is a tie!";
-    } else if (winningCombinations[choice1] === choice2) {
+    } else if (winningCombinations[userChoice] === computerChoice) {
         return "You win!";
     } else {
         return "You lose.";
     }
 };
-
-compare(userChoice, computerChoice)
