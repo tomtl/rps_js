60fa75195a17482d3ce529dc53824e04c45d73ee
Tom Lee <tom_t_lee@yahoo.com>
2015-10-27 18:02:02 +0000

    Add user choice validation

--- rps.js
+++ rps.js
@@ -1,20 +1,25 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?").toLowerCase();
 
-var computerChoice = Math.random();
+if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+    while (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+        var userChoice = prompt("Invalid input. Do you choose 'rock', 'paper' or 'scissors'?").toLowerCase();
+    }
+}
+
+var computerChoice = Math.random() * 3;
+if (computerChoice < 1) {
+	computerChoice = "rock";
+} else if(computerChoice < 2) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
 
 var winningCombinations = {
     rock: "scissors",
     scissors: "paper",
     paper: "rock"
 };
-
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
 
 var compare = function(choice1, choice2) {
     if (choice1 === choice2) {
